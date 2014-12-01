362153f023283d99579f7c33833910c4bcaa6064
idan <idan.frid@gmail.com>
2014-12-01 15:21:05 +0200

    Fixed Kafka's SI xml

--- src/main/java/com/idan/calcservice/controller/FacebookController.java
+++ src/main/java/com/idan/calcservice/controller/FacebookController.java
@@ -6,13 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
-import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
